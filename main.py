c4da7103440ee4d67779ee21f262f537098b558f
seanp97 <sean.pelser97@gmail.com>
2020-02-13 17:19:18 +0000

    Update main.py

--- main.py
+++ main.py
@@ -6,12 +6,17 @@ import tkinter
 from tkinter import messagebox
 from tkinter.simpledialog import askstring
 from tkinter import filedialog
+import time
 
 newFiles = ""
 saveFiles = ""
 textCode = ""
 folderSelected = ""
 existPath = ""
+folderFile = ""
+fOpenFile = ""
+Opened = False
+openString = ""
 
 def newFile():
     global folderSelected
@@ -28,11 +33,19 @@ def saveFile():
     global newFiles
     global existPath
     global folderSelected
+    global Opened
+    global folderFile
     if os.path.exists(existPath):
         textCode = textArea.get("1.0",END)
         fSaveFile = open(folderSelected + "/" + newFiles, "w")
         fSaveFile.write(textCode)
         messagebox.showinfo("Saved", newFiles +  " has been saved")
+
+    if Opened == True:
+        print(folderFile)
+        getText = textArea.get("1.0",END)
+        fOpenSave = open(folderFile, "w")
+        fOpenSave.write(getText)
     else:
         saveFolderSelected = filedialog.askdirectory()
         textCodeSave = textArea.get("1.0",END)
@@ -40,6 +53,18 @@ def saveFile():
         fNewSaveFile = open(saveFolderSelected + "/" + newSavedFiles, "w")
         fNewSaveFile.write(textCodeSave)
         messagebox.showinfo("Saved", newSavedFiles +  " has been saved")
+
+
+def openFile():
+    global Opened
+    Opened = True
+    global folderFile
+    global fOpenFile
+    global openString
+    folderFile = filedialog.askopenfilename()
+    fOpenFile = open(folderFile, "r")
+    openString = fOpenFile.read()
+    textArea.insert(END, openString)
 
 app = Tk()
 app.title("Text Editor")
@@ -52,6 +77,9 @@ newBtn.place(x=5, y=5)
 
 saveBtn = Button(app, text="Save", command=saveFile)
 saveBtn.place(x=45, y=4)
+
+openBtn = Button(app, text="Open", command=openFile)
+openBtn.place(x=85, y=4)
 
 textArea = Text(app, height=appHeight, width=appWidth)
 textArea.place(x=0, y=35)
